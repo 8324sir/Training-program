d653b1c83907882dd24ff2ab2bd56d556cd7cc91
wangyang <419468714@qq.com>
2017-04-23 00:37:10 +0800

    修复个BUG

--- Clock/clock.js
+++ Clock/clock.js
@@ -2,31 +2,31 @@ const hourHand = document.querySelector('.hour-hand');
 const minHand = document.querySelector('.minute-hand');
 const secondHand = document.querySelector('.second-hand');
 
-function step(){
+function step() {
   var now = new Date()
 
-var hours = now.getHours()
-var minutes = now.getMinutes()
-var seconds = now.getSeconds()
+  var hours = now.getHours()
+  var minutes = now.getMinutes()
+  var seconds = now.getSeconds()
 
-if(hours >12){
-  hours -= 12
-}
+  if (hours > 12) {
+    hours -= 12
+  }
 
-var secondsDeg = seconds / 60 * 360
-var minutesDeg = (minutes * 60 + seconds) / 3600 * 360
-var hoursDeg = (hours*3600 + minutes*60 + seconds) / (12*3600)*360
+  var secondsDeg = seconds / 60 * 360
+  var minutesDeg = (minutes * 60 + seconds) / 3600 * 360
+  var hoursDeg = (hours * 3600 + minutes * 60 + seconds) / (12 * 3600) * 360
 
-console.log(secondsDeg)
-console.log(minutesDeg)
-console.log(hoursDeg)
+  if (secondsDeg === 90) {
+    secondHand.style.transition = 'all 0s';
+  }
 
- hourHand.style.transform = `rotate(${-90+hoursDeg}deg)`;
- minHand.style.transform = `rotate(${-90+minutesDeg}deg)`;
- secondHand.style.transform = `rotate(${-90+secondsDeg}deg)`;
+  hourHand.style.transform = `rotate(${-90 + hoursDeg}deg)`;
+  minHand.style.transform = `rotate(${-90 + minutesDeg}deg)`;
+  secondHand.style.transform = `rotate(${-90 + secondsDeg}deg)`;
 }
 
 step()
-setInterval(function(){
+setInterval(function () {
   step()
-},1000)
+}, 1000)
