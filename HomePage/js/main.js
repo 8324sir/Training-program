f3c23964d9a06081da16cfeb2bf1decf69c1b0da
wangyang <419468714@qq.com>
2017-05-14 23:06:12 +0800

    基本框架写好了

--- HomePage/js/main.js
+++ HomePage/js/main.js
@@ -1,1 +1,16 @@
+$(function () {
+    //第一个轮播
+    $(".home-slides").slidesjs({
+        width: 960,
+        height: 528,
+        navigation: false,
+        pagination: false,
+        play: {
+            active: false,
+            interval: 3000,
+            auto: true,
+        }
+    });
 
+    //第二个轮播
+});
